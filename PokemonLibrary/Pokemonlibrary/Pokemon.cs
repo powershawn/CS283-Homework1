39518f8e000a822de07ad2732e1b33904c5819b3
powershawn <powershawnlin@gmail.com>
2017-07-14 01:30:13 +0800

    pokimo第一次上傳

---
+++ PokemonLibrary/Pokemonlibrary/Pokemon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemonlibrary
+{
+    public class Pokemon
+    {
+        public string Name;
+        public string Sex;
+        public string MaxHP;
+        public string NowHP;
+        public string Weight;
+        public string Type;
+        public string Height;
+        public string Power;
+        public string PowerCandy;
+        public string PowerUp;
+        public string PowerUpCandy;
+        public string Evolve;
+        public string Skill1;
+        public string Skill2;
+        public string Skill1Deplete;
+        public string Skill2Deplete;
+    }
+}
---
+++ PokemonLibrary/Pokemonlibrary/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemonlibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pokemon aspok = new Pokemon()
+            {
+                Name = "妙蛙種子",
+                Sex = "男",
+                MaxHP = "100",
+                NowHP = "66",
+
+                Weight = "77kg",
+                Type = "草系",
+                Power = "66666",
+                PowerCandy = "123",
+                PowerUp = "4444",
+                PowerUpCandy = "55",
+                Evolve = "111",
+                Skill1 = "藤鞭",
+                Skill2 = "汙泥炸彈",
+                Skill1Deplete ="7",
+                Skill2Deplete = "80"
+
+            };
+            Console.WriteLine("神奇寶貝名字:{0},性別:{1},最大血量:{2},現在血量:{3},重量:{4},屬性:{5},星星數:{6},糖果數:{7},升級需要的星星數:{8},升級需要的糖果數:{9},進化需要的星星數數:{10},技能1:{11},技能2:{12},技能消耗1:{13},技能消耗2:{14}"
+                , aspok.Name
+                , aspok.Sex
+                , aspok.MaxHP
+                , aspok.NowHP
+                , aspok.Weight
+                , aspok.Type
+                , aspok.Power
+                , aspok.PowerCandy
+                , aspok.PowerUp
+                , aspok.PowerUpCandy
+                , aspok.Evolve
+                , aspok.Skill1
+                , aspok.Skill2
+                , aspok.Skill1Deplete
+                , aspok.Skill2Deplete
+
+            );
+        }
+    }
+}
